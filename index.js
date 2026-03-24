202003e0651a228a2e940aea5cbd5075f8ba54fe
krishna ghogare <mnshghogare@gmail.com>
2026-03-24 18:06:38 +0530

    git ignore file added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
+import "dotenv/config";
 import express from "express";
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT||3000;
 
-// basioc get opeartions
+// basic get opeartions
 app.get("/", (req, res) => {
   res.send("do you want to become friend of cartel");
 });
@@ -39,9 +41,9 @@ app.get("/show", (req, res) => {
 // sending the data with particular id
 
 app.get("/show/:id", (req, res) => {
-  let curr =null;
+  let curr = null;
   // extrcting id from the URL and converting it from string to int
-  let id=Number(req.params.id);
+  let id = Number(req.params.id);
 
   // finding the object with given id
   for (let index = 0; index < moneyCollected.length; index++) {
@@ -53,7 +55,7 @@ app.get("/show/:id", (req, res) => {
   }
   // if we dont find object with given id
   if (!curr) {
-   res.status(404).send("data with that id dont exist");
+    res.status(404).send("data with that id dont exist");
   }
 
   // returning the response
@@ -61,57 +63,47 @@ app.get("/show/:id", (req, res) => {
 });
 
 // updaitng the data at given id
-app.put("/change/:id",(req,res)=>
-{
-    const {money}=req.body;
-    let id=Number(req.params.id);
-    let changed=null;
-    for(let i=0;i<moneyCollected.length;i++)
-    {
-      let elem=moneyCollected[i];
-      if(id===elem.id)
-      {
-        elem.money=money;
-        changed=elem;
-        break;
-      }
+app.put("/change/:id", (req, res) => {
+  const { money } = req.body;
+  let id = Number(req.params.id);
+  let changed = null;
+  for (let i = 0; i < moneyCollected.length; i++) {
+    let elem = moneyCollected[i];
+    if (id === elem.id) {
+      elem.money = money;
+      changed = elem;
+      break;
     }
-    if(!changed)
-    {
-     return  res.status(404).send("data with that given id dont exist")
-    }
-    res.status(201).send(changed);
-})
+  }
+  if (!changed) {
+    return res.status(404).send("data with that given id dont exist");
+  }
+  res.status(201).send(changed);
+});
 
 // deleting the data at given id
-app.delete("/delete/:id",(req,res)=>
-{
+app.delete("/delete/:id", (req, res) => {
   // extracting id from the URL
-  const id=(Number)(req.params.id);
-  let deletedAc=null;
-  for(let i=0;i<moneyCollected.length;i++)
-  {
+  const id = Number(req.params.id);
+  let deletedAc = null;
+  for (let i = 0; i < moneyCollected.length; i++) {
     // finding the element with the given id and delete it
-    let elem=moneyCollected[i];
-    if(id===elem.id)
-    {
-      deletedAc=elem;
-      moneyCollected.splice(i,1);
+    let elem = moneyCollected[i];
+    if (id === elem.id) {
+      deletedAc = elem;
+      moneyCollected.splice(i, 1);
       break;
     }
   }
 
   // if the element  with id is not found
-  if(!deletedAc)
-  {
+  if (!deletedAc) {
     return res.status(404).send("data with the given id dont exist");
   }
 
   // returning the responsez
   return res.status(200).send(deletedAc);
-
-
-})
+});
 app.get;
 app.listen(port, () => {
   console.log(`the server is listining to port ${port} ...`);
